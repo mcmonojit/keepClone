3b61e1de569f29629a351f9dbc97b7c44d5f8120
Monojit Chakraborty <cmonojit1999@gmail.com>
2021-08-09 10:19:23 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import HighlightIcon from "@material-ui/icons/Highlight";
+import EmojiObjectsRoundedIcon from '@material-ui/icons/EmojiObjectsRounded';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {/* <HighlightIcon /> */}
+        <EmojiObjectsRoundedIcon/>
+        Keep Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
